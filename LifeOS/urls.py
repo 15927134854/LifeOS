078f46ecc576b00931a2985effda94b9bdc9bfeb
leiwang <leiwang@pisx.com>
2025-05-19 16:03:17 +0800

    移除action 应用

--- LifeOS/urls.py
+++ LifeOS/urls.py
@@ -22,7 +22,6 @@ from django.urls import path, re_path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'goal/', include('goal.urls', namespace='goal')),
-    re_path(r'action/', include('action.urls', namespace='action')),
 
 ]
 
